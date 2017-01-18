098ade204b20c4636e08bceaaa16dfaaaefca9a8
gembuk <macanternak86@gmail.com>
2017-01-18 11:39:41 +0700

    wes

--- coba.php
+++ coba.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <tittle>sbc</tittle>
     <style type="text/css">
       html, body { height: 100%; margin: 0; padding: 0; }
       #map { height: 100%; }
@@ -14,7 +15,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 7.9826, lng: 112.6309},
-    zoom: 15
+    zoom: 15//selamat datang
   });
 }
 
